8a1d960674af52ce70b94011d423568879f93c1b
Nicolas Taillepierre <taillepierren@gmail.com>
2022-02-15 10:54:30 +0100

    change dbconfig pour simple module export

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,21 +1,15 @@
-const Sequelize = require("sequelize");
 
-
-const db = new Sequelize('groupomania', process.env.MYSQLROOT, process.env.MYSQLPASS, {
-    host: 'localhost',
+module.exports= {
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: 'root',
+    DB: `groupomania`,
     dialect: 'mysql',
     port: 3310,
     pool:{
-        max:100,
-        min: 0
-    },
-});
-
-db.authenticate().then(()=> {
-    console.log('Connection has been established successfully.');
-})
-  .catch ((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-module.exports = db;
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
