ade70de9b29436f80a74196b8c526844560bb7cb
Nicolas Taillepierre <taillepierren@gmail.com>
2022-02-15 10:56:38 +0100

    ajout controller signup et login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,21 +1,55 @@
+const db = require("../models");
+const User = db.users;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
-
-exports.signup = (req, res) => {
-    res.status(201);
-    console.log('route signup ok')
+exports.signup = async (req, res) => {
+    try {
+        const hash = await bcrypt.hash(req.body.password, 10)
+        const user = {
+            email: req.body.email,
+            password: hash,
+            username: req.body.username
+        };
+        await User.create(user)
+        return res.status(201).json({
+            message: "utilisateur crée"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
 }
 
-exports.login = (req, res) => {
-    res.status(200);
-    console.log('route login ok')
-}
-
-exports.ModifyUser = (req,res) => {
-    res.status(201);
-    console.log('route modifyUser ok')
-}
-
-exports.deleteUser =(req, res) => {
-    res.status(205)
-    console.log('route deleteAccount ok')
+exports.login = async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email
+        })
+        if (!user) {
+            return res.status(404).json({
+                error: "Utilisateur non trouvé"
+            });
+        }
+        const isValid = await bcrypt.compare(req.body.password, user.password)
+        if (!isValid) {
+            return res.status(400).json({
+                error: "Mot de passe incorrect"
+            });
+        }
+        res.status(200).json({
+            userId: user.id,
+            token: jwt.sign({
+                    userId: user.id
+                },
+                process.env.TOKEN, {
+                    expiresIn: '24h'
+                })
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
 }
