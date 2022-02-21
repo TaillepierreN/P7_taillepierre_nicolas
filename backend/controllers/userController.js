9747ad7a3c1c55943882bea6284609df12066757
Nicolas Taillepierre <taillepierren@gmail.com>
2022-02-21 11:46:38 +0100

    ajout userController,update userRouting

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,36 @@
 const db = require("../models");
 const User = db.users;
 
-exports.ModifyUser = (req,res) => {
-    res.status(201);
-    console.log('route modifyUser ok')
+exports.ModifyUser = async (req,res) => {
+    try {
+        const user = User.findOne({where:{ id: req.params.id}})
+        if(user){
+            await User.update({
+                email: req.body.email,
+                username: req.body.username,
+                profilepic: `${req.protocol}://${req.get('host')}//images/${req.file.filename}`},
+                {where:{id: req.params.id}})
+                return res.status(200).json({
+                    message: ' Profile mis à jour'
+                });
+            }
+    } catch (error) {
+        return res.status(400).json({ error: error});
+    }
 }
 
 exports.deleteUser =(req, res) => {
-    res.status(205)
-    console.log('route deleteAccount ok')
+    try {
+        const user = User.findOne({where:{ id: req.params.id}})
+        if(user){
+            User.destroy({where: {id: req.params.id}})
+            return res.status(200).json({
+                message: 'Profile retiré'
+            })
+        }
+
+    } catch (error) {
+        return res.status(400).json({ error: error});
+    }
+
 }
--- backend/routes/userRouting.js
+++ backend/routes/userRouting.js
@@ -1,12 +1,13 @@
 const {Router} = require('express');
 const { ModifyUser, deleteUser } = require('../controllers/userController');
 const auth = require('../middlewares/auth');
+const isOwnerOrAdmin = require('../middlewares/isOwnerOrAdmin');
 const multer = require('../middlewares/multer-config');
 
 const router = Router();
 
-router.put('/:id', auth, multer, ModifyUser )
-router.delete('/:id', auth, deleteUser)
+router.put('/:id', auth, isOwnerOrAdmin, multer, ModifyUser )
+router.delete('/:id', auth, isOwnerOrAdmin, deleteUser)
 
 
 module.exports = router;
