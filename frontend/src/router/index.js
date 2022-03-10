a475dc654a36621638182bc755f027903e0cc8a8
Nicolas Taillepierre <taillepierren@gmail.com>
2022-03-10 08:58:43 +0100

    route frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 const routes = [
   {
@@ -8,18 +10,26 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+// router.beforeEach((to, from, next) => {
+//   if ((to.name !== 'Login' && to.name !== 'Signup') && !localStorage.getItem('token')) {
+//     next({ name: 'Login' })
+//   }
+//   else next()
+// })
 
 export default router
