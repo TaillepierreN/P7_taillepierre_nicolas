3f60c930bbd01c0b0c40300b84f3adda01b73ac7
Nicolas Taillepierre <taillepierren@gmail.com>
2022-04-06 13:27:21 +0200

    fix multer pathing

--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -3,15 +3,21 @@ const multer = require('multer');
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png'
+    'image/png': 'png',
+    'image/webp': 'webp'
 }
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) =>{
-        callback(null, 'images')
+        if(req.body.type == "post"){
+        callback(null, 'images/attachment')
+        } else {
+        callback(null, 'images/profile')
+
+        }
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        const name = file.originalname.split(' ').join('_').split('.').join('_');
         const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
